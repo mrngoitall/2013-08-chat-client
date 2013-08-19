a642c4ce5ad7d3c9c72b2ffd7a348fda23f3e08f
Hack Reactor Students <github@hackreactor.com>
2013-08-19 15:43:28 -0700

    Added username and message input boxes

--- js/setup.js
+++ js/setup.js
@@ -1,39 +1,38 @@
-if(!/(&|\?)username=/.test(window.location.search)){
-  var newSearch = window.location.search;
-  if(newSearch !== '' & newSearch !== '?'){
-    newSearch += '&';
-  }
-  newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
-  window.location.search = newSearch;
-}
+$(document).ready(function() {
+  // Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
+  $.ajaxPrefilter(function(settings, _, jqXHR) {
+    jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
+    jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
+  });
 
-// Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
-$.ajaxPrefilter(function(settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
-});
-
-$.ajax({
-  type: 'POST',
-  contentType: 'application/json',
-  url: 'https://api.parse.com/1/classes/messages',
-  data:  '{ "text": "this is a awesome test", \
-    "username":"The Master" }'
-});
-
-$.ajax('https://api.parse.com/1/classes/messages?order=-createdAt', {
-  contentType: 'application/json',
-  cache: false,
-  success: function(data){
-    for (var i = 0; i < data.results.length; i++) {
-      $tweet = $('<p></p>');
-      $tweet.append($("<span class='user'></span>").text(data.results[i].username+": "));
-      $tweet.append($("<span class='message'></span>").text(data.results[i].text));
-      $('#main').append($tweet);
+  $.ajax('https://api.parse.com/1/classes/messages?order=-createdAt', {
+    contentType: 'application/json',
+    cache: false,
+    success: function(data){
+      for (var i = 0; i < data.results.length; i++) {
+        $tweet = $('<p></p>');
+        $tweet.append($("<span class='user'></span>").text(data.results[i].username+": "));
+        $tweet.append($("<span class='message'></span>").text(data.results[i].text));
+        $('#main').append($tweet);
+      }
+      console.log(data);
+    },
+    error: function(data) {
+      console.log('Ajax request failed');
     }
-    console.log(data);
-  },
-  error: function(data) {
-    console.log('Ajax request failed');
-  }
+  });
+
+  $('#submit').on('click', function() {
+    if($("#username").val() === ''){
+      return alert('Please enter a username!');
+    }
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: 'https://api.parse.com/1/classes/messages',
+      data: '{ "text": '+JSON.stringify($("#message").val())+', \
+        "username":'+JSON.stringify($("#username").val())+' }'
+    });
+  });
+
 });
