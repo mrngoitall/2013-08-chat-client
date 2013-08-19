f17efc31490d4c215befd1e55561a0a04a6a9e18
Hack Reactor Students <github@hackreactor.com>
2013-08-19 12:33:17 -0700

    successfully retrieved and displayed messages

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,24 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+$.ajax({
+  type: 'POST',
+  contentType: 'application/json',
+  url: 'https://api.parse.com/1/classes/messages',
+  data:  '{ "text": "this is a awesome test" }'
+});
+
+$.ajax('https://api.parse.com/1/classes/messages', {
+  contentType: 'application/json',
+  cache: false,
+  success: function(data){
+    for (var i = 0; i < data.results.length; i++) {
+      $('#main').append($("<p></p>").text(data.results[i].text));
+    }
+    console.log(data);
+  },
+  error: function(data) {
+    console.log('Ajax request failed');
+  }
+});
