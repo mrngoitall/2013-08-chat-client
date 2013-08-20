e1771d20813335eab1b1afca9b99334ee969310a
Hack Reactor Students <github@hackreactor.com>
2013-08-19 20:45:38 -0700

    added support for changing rooms

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var friends = {};
   var currentRoom = 'messages';
+  var chatRooms = {'messages': true};
   // Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
   $.ajaxPrefilter(function(settings, _, jqXHR) {
     jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
@@ -8,6 +9,7 @@ $(document).ready(function() {
   });
 
   var refreshMessages = function() {
+
     $.ajax('https://api.parse.com/1/classes/'+currentRoom+'?order=-createdAt', {
       contentType: 'application/json',
       cache: false,
@@ -20,19 +22,25 @@ $(document).ready(function() {
           $('#messages').append($tweet);
         }
         $('.user').on('click', function(){
-          var friend = '<p>' + $(this).text().slice(0,$(this).length - 3) + '</p>';
-          if(friends[friend] === undefined) {
+          var friendUsername = $(this).text().slice(0,$(this).length - 3);
+          var friend = '<p>' + friendUsername + '</p>';
+          if(friends[friendUsername] === undefined) {
             $('#friends').append(friend);
-            $('.'+$(this).text().slice(0,$(this).length - 3)).css({'font-weight':'bold'});
+            $('.'+friendUsername).css({'font-weight':'bold'});
           }
-          friends[friend] = true;
+          friends[friendUsername] = true;
         });
         console.log(data);
+        for(var key in friends){
+          console.log(key);
+          $('.'+key).css({'font-weight':'bold'});
+        }
       },
       error: function(data) {
         console.log('Ajax request failed');
       }
     });
+
   };
   refreshMessages();
 
@@ -61,6 +69,27 @@ $(document).ready(function() {
     if ( event.which == 13 ) {
        sendMessage();
      }
+  });
+
+  $('#chooseroom').change(function(e) {
+    if(this.value === 'createRoom'){
+      var newRoomName = prompt('What room did you want to join?') || 'messages';
+      if(chatRooms[newRoomName] === undefined){
+        chatRooms[newRoomName] = true;
+        var chooseRoomOptions = "";
+        for(var key in chatRooms){
+          if(key === newRoomName){
+            chooseRoomOptions += '<option value="' + key + '" selected>' + key + '</option>';
+          } else {
+            chooseRoomOptions += '<option value="' + key + '">' + key + '</option>';
+          }
+        }
+        chooseRoomOptions += '<option value="createRoom">Create a room</option>';
+        $('#chooseroom').html(chooseRoomOptions);
+      }
+    }
+    currentRoom = newRoomName || this.value;
+    refreshMessages();
   });
 
 });
